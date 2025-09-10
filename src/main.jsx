6db6849f56169f9d5b278cadaf0707a59de8f5f9
rafaelmacielusa-wq <rafaelmaciel.usa@gmail.com>
2025-09-10 16:22:14 -0300

    Update main.tsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-createRoot(document.getElementById('root')).render(<App />)
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./index"; // seu componente default do canvas
+
+const root = createRoot(document.getElementById("root")!);
+root.render(<App />);
