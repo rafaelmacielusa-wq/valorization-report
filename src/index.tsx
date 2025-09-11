a8a0ba57833f45be01b373a5269b0d4a4d1b0ab8
rafaelmacielusa-wq <rafaelmaciel.usa@gmail.com>
2025-09-11 14:08:00 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,1 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import ValorizationReportApp from "./ValorizationReportApp";
-
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ValorizationReportApp />
-  </React.StrictMode>
-);
+export { default } from "./ValorizationReportApp";
