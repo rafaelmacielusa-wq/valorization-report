77bd8dbedb5947d0a9ef7eb3ad62dac65ee3ac89
rafaelmacielusa-wq <rafaelmaciel.usa@gmail.com>
2025-09-12 16:17:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  base: "./",
+  server: { host: true, port: 5173 },
+  build: { target: "es2021", outDir: "dist", sourcemap: false }
 });
