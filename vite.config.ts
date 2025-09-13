881c30531fddf57bcbaebd44c90a0c3461d5d11a
rafaelmacielusa-wq <rafaelmaciel.usa@gmail.com>
2025-09-13 09:51:33 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  server: { host: true, port: 5173 },
-  build: { target: "es2021", outDir: "dist", sourcemap: false }
+  // Se você precisar publicar em subpasta, ajuste o "base".
+  // base: '/minha-subpasta/'
 });
