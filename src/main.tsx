4fb4a58afd35d1610e0e8b7508e5fe0997d24714
rafaelmacielusa-wq <rafaelmaciel.usa@gmail.com>
2025-09-13 09:51:52 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./ValorizationReportApp";
+import ValorizationReportApp from "./ValorizationReportApp";
 
 const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <ValorizationReportApp />
+  </React.StrictMode>
+);
